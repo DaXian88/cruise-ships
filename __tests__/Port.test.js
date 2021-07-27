15e463ec7bb66c704c64a41f56b735b75ef58fbb
Khalid Safy <khalid_safy88@hotmail.com>
2021-07-27 22:23:30 +0100

    DRY'd up the test suites, included beforeEach where possible

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -3,30 +3,47 @@ const Port = require('../src/Port.js');
 const Ship = require('../src/Ship.js');
 
 describe('Port', () => {
-    it('can be instantiated', () => { 
-        const port = new Port('Dublin');
-        expect(port).toBeInstanceOf(Object)
-    })
-    it('has a name property', () => {
-        const port = new Port('Dublin');
-        expect(port).toHaveProperty('name')
-    })
+    describe('takes a port and ship', () => {
+        let dublin; 
+        let liverpool; 
+        let itinerary; 
+        let titanic; 
+        
+        beforeEach(() => {
+            liverpool = new Port('Liverpool');
+            dublin = new Port('Dublin');
+            itinerary = new Itinerary([liverpool, dublin]);
+            titanic = new Ship(itinerary);
+        }); 
+        it('can be instantiated', () => { 
+
+        expect(dublin).toBeInstanceOf(Object)
+        })
+
+        it('has a name property', () => {
+
+        expect(dublin).toHaveProperty('name')
+        })
+    });
+    
     it('adds a ship to the port', () => {
-        const dublin = new Port('Dublin');
-        const liverpool = new Port('Liverpool');
-        const itinerary = new Itinerary([liverpool, dublin]);
         const ship = {};
+        const liverpool = new Port('Liverpool')
+
         liverpool.addShip(ship);
         expect(liverpool.ships).toEqual([ship]);
-    })
+        });
+
     it('removes a ship from a port', () => {
-        const dublin = new Port('Dublin');
         const liverpool = new Port('Liverpool');
+        const dublin = new Port('Dublin');
         const itinerary = new Itinerary([liverpool, dublin]);
         const titanic = new Ship(itinerary);
-        liverpool.addShip(titanic);
+
         liverpool.removeShip(titanic);
         expect(liverpool.ships).toEqual([])
-    })
+        });
+   
+   
 
-})
+});
--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,52 +4,49 @@ const Port = require('../src/Port.js');
 const Ship = require('../src/Ship.js');
 
 describe('Ship', () => {
-    it('can be instantiated', () => {
-        const liverpool = new Port('Liverpool');
-        const itinerary = new Itinerary([liverpool]);
-        const titanic = new Ship(itinerary);
+    describe('with ports and an itinerary', () => {
+        let liverpool;
+        let dublin;
+        let itinerary;
+        let titanic;
+
+        beforeEach(() => {
+            liverpool = new Port('Liverpool');
+            dublin = new Port('Dublin');
+            itinerary = new Itinerary([liverpool, dublin]);
+            titanic = new Ship(itinerary);
+        }); 
+        it('can be instantiated', () => {
+
         expect(titanic).toBeInstanceOf(Object);
-    })
-    it('has a starting port', () => {
-        const liverpool = new Port('Liverpool');
-        const itinerary = new Itinerary([liverpool]);
-        const titanic = new Ship(itinerary);
+        });
+
+        it('has a starting port', () => {
 
         expect(titanic.currentPort).toBe(liverpool)
-    })
-    it('can set sail', () => {
-        const liverpool = new Port('Liverpool');
-        const dublin = new Port('Dublin');
-        const itinerary = new Itinerary([liverpool, dublin]);
-        const titanic = new Ship(itinerary);
+        });
+        
+        it('can set sail', () => {
         titanic.setSail();
 
         expect(titanic.currentPort).toBeFalsy();
         expect(liverpool.ships).not.toContain(titanic)
-    })
-    it('docks at a different port', () => {
-        const liverpool = new Port('Liverpool');
-        const dublin = new Port('Dublin');
-        const itinerary = new Itinerary([liverpool, dublin]);
-        const ship = new Ship(itinerary);
+        });
+        
+        it('docks at a different port', () => {
+     
+        titanic.setSail()
+        titanic.dock()
 
-        ship.setSail()
-        ship.dock()
-
-        expect(ship.currentPort).toBe(dublin)
-    })
-    it('gets added to port on instantiation', () => {const liverpool = new Port('Liverpool');
-        const dublin = new Port('Dublin');
-        const itinerary = new Itinerary([liverpool, dublin]);
-        const titanic = new Ship(itinerary);
+        expect(titanic.currentPort).toBe(dublin)
+        });
+        
+        it('gets added to port on instantiation', () => {
 
         expect(liverpool.ships).toContain(titanic);
-    })
-    it('can dock at a different port', () => {
-        const liverpool = new Port('Liverpool');
-        const dublin = new Port('Dublin');
-        const itinerary = new Itinerary([liverpool, dublin]);
-        const titanic = new Ship(itinerary);
+        });
+        
+        it('can dock at a different port', () => {
 
         titanic.setSail();
         titanic.dock();
@@ -57,6 +54,9 @@ describe('Ship', () => {
         expect(titanic.currentPort).toEqual(dublin);
         expect(dublin.ships).toContain(titanic)
         
-    })
+        });
+    });
+
+   
 
 });
